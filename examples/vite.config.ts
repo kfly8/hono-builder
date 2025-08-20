5d4f62adec08df27b688aaba4b3647b96de2280a
kobaken <kentafly88@gmail.com>
2025-08-21 07:49:31 +0900

    Simplify examples/vite.config.ts

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,12 +1,37 @@
 import { defineConfig } from 'vite'
 import devServer from '@hono/vite-dev-server'
-import { createHotUpdateHandler } from './vite-utils'
+import type { HmrContext, ModuleNode } from 'vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [
     devServer({
       entry: 'src/server.ts',
-      handleHotUpdate: createHotUpdateHandler(['src/builder.ts']),
+      handleHotUpdate: ({ server, modules }: HmrContext): void | ModuleNode[] => {
+        const isSSR = modules.some((mod) => (mod as any)._ssrModule)
+
+        if (isSSR) {
+          const modulesToReload = ['src/builder.ts']
+
+          for (const modulePath of modulesToReload) {
+            const absolutePath = path.join(__dirname, modulePath)
+            const module = server.moduleGraph.getModuleById(absolutePath)
+
+            if (module) {
+              server.reloadModule(module)
+            }
+            else {
+              console.log(`Module not found for hot reload: ${absolutePath}`)
+            }
+          }
+
+          server.hot.send({ type: 'full-reload' })
+          return []
+        }
+      },
     })
   ]
 })
